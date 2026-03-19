46cc16e2e4ae23f480bee035b564d0f01a221671
Thomas F <thomas.faccin95@gmail.com>
2026-03-19 21:05:09 +0100

    Fetch: Removed commented out code in PublicController homepage method

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,5 +9,9 @@ class PublicController extends Controller
     public function homepage()
     {
         return view('welcome');
+        // $articles = Article::take(6)->orderBy('create_at', 'desc')->get();
+        // return view('welcome', compact('articles'));
     }
+
+    
 }

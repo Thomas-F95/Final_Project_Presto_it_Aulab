e3e4fdcc22b381364c474f5c65a4e2b80c102f98
Thomas F <thomas.faccin95@gmail.com>
2026-03-20 19:51:52 +0100

    Fetch: Add new file index.blade to folder article

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+
+
+<div class="container-fluid">
+    <div class="row height-custom justify-content-center align-items-center text-center ">
+        <div class="col-12">
+            <h1 class="display-1">Tutti gli articoli</h1>
+        </div>
+    </div>
+</div>
+
+</x-layout>
